5a388bd01ed1836b2f2e9f6e34338aacc10308ad
liquan <liquan_0108@163.com>
2024-03-21 18:09:21 +0800

    test: supp. 'retry-cdn.js' test case

--- test/retry-cdn.test.js
+++ test/retry-cdn.test.js
@@ -54,3 +54,145 @@ describe('test retryJsSrc state true', () => {
         expect(Util.createScript).toHaveBeenCalledWith(resultUrl,true,false)
     });
 });
+
+describe('test retryLinkSrc', () => {
+    let retryCDN;
+    const resultUrl = 'https://cdnjs.cloudflare.com/ajax/libs/element-plus/2.6.1/index.min.css'
+    beforeEach(()=>{
+        retryCDN = new RetryCDN([
+            'https://cdnjs.cloudflare.com',
+            'https://images.unsplash.com',
+            'https://plus.unsplash.com',
+        ])
+    })
+
+    it('Switch CDN for link', () => {
+        const target = {
+            href: 'https://wwwww/ajax/libs/element-plus/2.6.1/index.min.css'
+        }
+        retryCDN.retryLinkSrc(target);
+        expect(Util.createLink).toHaveBeenCalledWith(resultUrl);
+    });
+});
+
+describe('test retryImgSrc', () => {
+    let retryCDN;
+    beforeEach(()=>{
+        retryCDN = new RetryCDN([
+            'https://images.unsplash.com',
+            'https://cdnjs.cloudflare.com',
+            'https://plus.unsplash.com',
+        ])
+    })
+
+    it('Switch CDN for img', () => {
+        const target = {
+            src:'https://9.unsplash.com/photo-1525480122447-64809d765ec4'
+        }
+        const resultUrl = 'https://images.unsplash.com/photo-1525480122447-64809d765ec4'
+
+        retryCDN.retryImgSrc(target)
+        expect(target.src).toEqual(resultUrl)
+    });
+});
+
+describe('test setErrorMap', () => {
+    let retryCDN;
+    const cdnArr = [
+        'https://images.unsplash.com',
+        'https://cdnjs.cloudflare.com',
+        'https://plus.unsplash.com',
+    ]
+    beforeEach(()=>{
+        retryCDN = new RetryCDN(cdnArr)
+    })
+    
+    const target = {}
+    const errorMap = new Map()
+
+    it('each every branch', () => {
+        const expectResultArr = [
+            {
+                state: true,
+                num: 1
+            },
+            {
+                state: true,
+                num: 2
+            },
+            {
+                state: true,
+                num: 3
+            },
+            {
+                state: false,
+                num: 3
+            }
+        ];
+        expectResultArr.forEach(item => {
+            const result = retryCDN.setErrorMap(errorMap,target)
+            expect(result).toEqual(item)
+        });
+    });
+});
+
+describe('test hasRule', () => {
+    let retryCDN;
+    const cdnArr = [
+        'https://images.unsplash.com',
+        'https://cdnjs.cloudflare.com',
+        'https://plus.unsplash.com',
+    ]
+    beforeEach(()=>{
+        retryCDN = new RetryCDN(cdnArr);
+    })
+
+    it('has rules', () => {
+        const styleSheets = {
+            rules:'rules',
+            cssRules:'cssRules',
+        }
+        const result = retryCDN.hasRule(styleSheets);
+        expect(styleSheets.rules).toEqual('rules')
+        expect(result).toBeTruthy()
+    });
+
+    it('no rules', () => {
+        const styleSheets = {
+            cssRules:'cssRules',
+        };
+        const result = retryCDN.hasRule(styleSheets);
+        expect(styleSheets.rules).toEqual('cssRules')
+        expect(result).toBeTruthy();
+    });
+
+    
+    it('no cssRules and cssRules', () => {
+        let styleSheets;
+        const result = retryCDN.hasRule(styleSheets);
+        expect(result).toBeNull();
+    });
+});
+
+describe('test getAllUrlArr', () => {
+    let retryCDN;
+    const cdnArr = [
+        'https://images.unsplash.com',
+        'https://cdnjs.cloudflare.com',
+        'https://plus.unsplash.com',
+    ]
+    beforeEach(()=>{
+        retryCDN = new RetryCDN(cdnArr);
+    })
+
+    it('get url array', () => {
+        const bgUrl = 'https://00.unsplash/premium_photo-1673603988195-1253725273e8';
+        const expectArr = [
+            'https://images.unsplash.com/premium_photo-1673603988195-1253725273e8',
+            'https://cdnjs.cloudflare.com/premium_photo-1673603988195-1253725273e8',
+            'https://plus.unsplash.com/premium_photo-1673603988195-1253725273e8',
+        ]
+        const result = retryCDN.getAllUrlArr(bgUrl);
+        expect(result).toEqual(expectArr)
+    });
+});
